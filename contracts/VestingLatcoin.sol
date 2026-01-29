05c82cf363f8a04600434c7f8af945faef7fa589
Latcoin <latcoin@latbroker.com>
2026-01-28 22:00:11 -0500

    Update VestingLatcoin.sol

--- contracts/VestingLatcoin.sol
+++ contracts/VestingLatcoin.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
 
+import "@openzeppelin/contracts/finance/VestingWallet.sol";
+
+contract VestingLATC is VestingWallet {
+
+    constructor()
+        VestingWallet(
+            0x8505623eaDbD38eE1f54DF9DF8a653AFFd76c4Cd,
+            1794940800,
+            94608000
+        )
+    {}
+}
